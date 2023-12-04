55db41bcfd8663ee252cfd7b9d5dfff55ffee9fa
Ariabreu <135667003+Ariabreu@users.noreply.github.com>
2023-12-04 20:07:08 -0300

    a

--- css/main.js
+++ css/main.js
@@ -1,4 +1,9 @@
 const from = document.getElementById("novoItem")
 
-form.addEventlistener("submit", (evento)=>(
-    evento.prevent
+form.addEventlistener("submit",(evento)=>{
+    evento.preventDefault()
+
+   evento.log(evento)
+   evento.log(evento.target[0].value) 
+   evento.log(evento.target[1].value)  
+})
